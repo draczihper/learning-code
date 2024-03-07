b2e7a3eee0772701f3b0c240af3a5c7de8809bf4
Drac Zihper <draczihper@gmail.com>
2024-03-07 23:25:13 +0300

    Test code on regex execute returning a date object

--- webdev/script.js
+++ webdev/script.js
@@ -1,1 +1,7 @@
-let regex = /\/\/comment/;
+function getDate(string) {
+    let [_, month, day, year] = /(\d{1,2})-(\d{1,2})-(\d{4})/.exec(string);
+    return new Date(year, month - 1, day);
+}
+
+console.log(getDate("5-31-2003"));
+
