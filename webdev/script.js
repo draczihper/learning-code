dfc8ee26265025f03a66f9c529242331e62cab01
Drac Zihper <draczihper@gmail.com>
2024-03-18 21:36:53 +0300

    Test a DOM create element function

--- webdev/script.js
+++ webdev/script.js
@@ -1,14 +1,15 @@
-function talksAbout(node, string) {
-    if (node.nodeType == Node.ELEMENT_NODE) {
-        for (let child of node.childNodes) {
-            if (talksAbout(child, string)) {
-                return true;
-            }
-        }
-        return false;
-    } else if (node.nodeType == Node.TEXT_NODE) {
-        return node.nodeValue.indexOf(string) > -1;
+function elt(type, ...children) {
+    let node = document.createElement(type);
+    for (let child of children) {
+        if (typeof child != "string") node.appendChild(child);
+        else node.appendChild(document.createTextNode(child));
     }
+    return node;
 }
 
-console.log(talksAbout(document.body, "book"));
+document.getElementById("quote").appendChild(
+    elt("footer", "-",
+    elt("strong", "Karl Popper"),
+    ", preface to the second edition of ",
+    elt("em", "The Open Society and Its Enemies"),
+    ", 1950"));
