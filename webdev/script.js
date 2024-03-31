5eeb70f67fb3b091485e6eeea5e9cf93bcff9fcf
Drac Zihper <draczihper@gmail.com>
2024-03-31 21:55:31 +0300

    Test a function to encode URI

--- webdev/script.js
+++ webdev/script.js
@@ -1,8 +1,1 @@
-let bombTimer = setTimeout(() => {
-    console.log("BOOM");
-}, 500);
-
-if (Math.random() < 0.5) {
-    console.log("Defused.");
-    clearTimeout(bombTimer);
-}
+console.log(encodeURIComponent("Yes"));
