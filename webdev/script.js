bc4623bea9888f968a9f1bfe66b757f7cb6ada5f
Drac Zihper <draczihper@gmail.com>
2024-04-03 13:17:32 +0300

    Add a PixelEditor class

--- webdev/script.js
+++ webdev/script.js
@@ -1,1 +1,21 @@
-fetch("/example.txt").then(response => response.text()).then(text => console.log(text));
+class PixelEditor {
+    constructor(state, config) {
+        let {tools, controls, dispatch} = config;
+        this.state = state;
+        
+        this.canvas = new PictureCanvas(state.picture, pos => {
+            let tool = tools[this.state.tool];
+            let onMove = tool(pos, this.state, dispatch);
+            if(onMove) return pos => onMove(pos, this.state);
+        });
+        this.controls = controls.map(
+            Control => new Control(state, config)
+        );
+        this.dom = elt("div", {}, this.canvas.dom, elt("br"), ...this.controls.reduce((a, c) => a.concat(" ", c.dom), []));
+    }
+    syncState(state) {
+        this.state = state;
+        this.canvas.syncState(state.picture);
+        for (let ctrl of this.controls) ctrl.syncState(state);
+    }
+}
