a96b500a29b9c1cc3ce57c2685dca8b91200547f
Drac Zihper <draczihper@gmail.com>
2024-04-06 16:26:09 +0300

    Add code for fs module buffer object

--- webdev/script.js
+++ webdev/script.js
@@ -1,5 +1,6 @@
-let {readFile} = require("fs");
-readFile("file.txt", "utf8", (error, text) => {
+const {readFile} = require("fs");
+readFile("file.txt", (error, buffer) => {
 if (error) throw error;
-console.log("The file contains:", text);
+console.log("The file contained", buffer.length, "bytes.",
+"The first byte is:", buffer[0]);
 });
