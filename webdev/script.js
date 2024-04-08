05516f41d9f737bfb35556f731de0adf616386e8
Drac Zihper <draczihper@gmail.com>
2024-04-08 19:22:53 +0300

    Add code for http client

--- webdev/script.js
+++ webdev/script.js
@@ -1,10 +1,10 @@
-const {createServer} = require("http");
-let server = createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(`
-    <h1>Hello!</h1>
-    <p>You asked for <code>${request.url}</code></p>`);
-    response.end();
+const {request} = require("http");
+let requestStream = request({
+    hostname: "eloquentjavascript.net",
+    path: "/20_node.html", 
+    method: "GET",
+    headers: {Accept: "text/html"}
+}, response => {
+    console.log("Server responded with status code", response.statusCode);
 });
-server.listen(8000);
-console.log("Listening! (port 8000)");
+requestStream.end();
