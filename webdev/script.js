bfacbfc851354556b4f9fd2689ded2f097f00ebd
Drac Zihper <draczihper@gmail.com>
2024-04-10 19:33:27 +0300

    Add code to delete a file in server

--- webdev/script.js
+++ webdev/script.js
@@ -1,20 +1,15 @@
-const {createReadStream} = require("fs");
-const {stat, readdir} = require("fs").promises;
-const mime = require("mime");
+const {rmdir, unlink} = require("fs").promises;
 
-methods.GET = async function(request) {
+methods.DELETE = async function(request) {
     let path = urlPath(request.url);
     let stats;
     try {
-        stats = await stat(path);
+        stats = await stats(path);
     } catch (error) {
         if (error.code != "ENOENT") throw error;
-        else return {status: 404, body: "File not found"};
+        else return {status: 204};
     }
-    if (stats.isDirectory()) {
-        return {body: (await readdir(path)).join("\n")};
-    } else {
-        return {body: createReadStream(path),
-        type: mime.getType(path)};
-    }
+    if (stats.isDirectory()) await rmdir(path);
+    else await unlink(path);
+    return {status: 204};
 };
