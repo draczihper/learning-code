3816db9b389c80f666a256aa2f05cc5ec8e4fda1
Drac Zihper <draczihper@gmail.com>
2024-04-10 19:26:28 +0300

    Add code to look for a file in a dir

--- webdev/script.js
+++ webdev/script.js
@@ -1,10 +1,20 @@
-const {request} = require("http");
-let requestStream = request({
-    hostname: "eloquentjavascript.net",
-    path: "/20_node.html", 
-    method: "GET",
-    headers: {Accept: "text/html"}
-}, response => {
-    console.log("Server responded with status code", response.statusCode);
-});
-requestStream.end();
+const {createReadStream} = require("fs");
+const {stat, readdir} = require("fs").promises;
+const mime = require("mime");
+
+methods.GET = async function(request) {
+    let path = urlPath(request.url);
+    let stats;
+    try {
+        stats = await stat(path);
+    } catch (error) {
+        if (error.code != "ENOENT") throw error;
+        else return {status: 404, body: "File not found"};
+    }
+    if (stats.isDirectory()) {
+        return {body: (await readdir(path)).join("\n")};
+    } else {
+        return {body: createReadStream(path),
+        type: mime.getType(path)};
+    }
+};
