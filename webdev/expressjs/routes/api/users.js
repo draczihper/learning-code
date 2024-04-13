57ad5d8d2c976065c9d63b55baf036e2df67f5f3
Drac Zihper <draczihper@gmail.com>
2024-04-14 00:09:33 +0300

    Add update and delete user functionality

--- webdev/expressjs/routes/api/users.js
+++ webdev/expressjs/routes/api/users.js
@@ -37,8 +37,34 @@ router.post('/', (req, res) => {
 
 // Update user
 router.put('/:id', (req, res) => {
-    const found = users.some(user => user.)
-})
+    const found = users.some(user => user.id === parseInt(req.params.id))
+
+    if (found) {
+        const updateUser = req.body;
+        users.forEach(user => {
+            if (user.id === parseInt(req.params.id)) {
+                user.name = updateUser.name ? updateUser.name: user.name;
+                user.email = updateUser.email ? updateUser.email: user.email
+                res.json({msg: "User updated", user});
+            }
+        });
+    }
+});
+
+
+// Delete user
+router.delete('/:id', (req, res) => {
+    const found = users.some(user => user.id === parseInt(req.params.id))
+    if (found) {
+        users = users.filter(user => user.id !== parseInt(req.params.id));
+        res.json({
+            msg: "User deleted",
+            users,
+        });
+    } else {
+        res.sendStatus(400);
+    }
+});
 
 
 
