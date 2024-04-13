2fd27d61b010f14248e2afd15d75a722c4d45c7a
Drac Zihper <draczihper@gmail.com>
2024-04-13 11:03:28 +0300

    Test code foran expressjs app

--- webdev/nodejs/script.js
+++ webdev/nodejs/script.js
@@ -1,5 +1,13 @@
-var http = require('http');
-http.createServer(function(req, res) {
-    res.write("Hello world! Welcome to node js")
-    res.end()
-}).listen(8080);
+var express = require('express');
+var app = express();
+
+app.get('/',function(req, res) {
+    res.send("Hello world! Welcome to node js")
+})
+
+var server = app.listen(8080, function() {
+    var host = server.address().address;
+    var port = server.address().port
+
+    console.log("Example app listening at http://%s:%s", host, port);
+});
