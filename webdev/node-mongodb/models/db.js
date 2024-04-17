40900d5d51f4ac3005a9a90c6d95d80278e1e68b
Drac Zihper <draczihper@gmail.com>
2024-04-17 10:40:53 +0300

    Modify the database

--- webdev/node-mongodb/models/db.js
+++ webdev/node-mongodb/models/db.js
@@ -2,13 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost:27017/StudentDB', {
     useNewUrlParser: true
-},
-err => {
-    if(!err) {
-        console.log("Connection succeeded");
-    } else {
-        console.log("Error in connection" + err);
-    }
-});
+}).then(() => console.log('Connected successfully')).catch((err) => {console.log("Error in connection"+ err);});
 
 require('./student.model')
