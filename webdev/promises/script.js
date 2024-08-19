db821a89dcac71d1ccb6902714a9a7fa95568f8c
Drac Zihper <draczihper@gmail.com>
2024-08-19 23:51:37 +0300

    Add mock async

--- webdev/promises/script.js
+++ webdev/promises/script.js
@@ -1,18 +1,7 @@
-
-var p = new Promise(function(resolve, reject) {
-    /* Do an async task and then... */
-    
-    if(/* good condition */) {
-        resolve('Success');
-    } else {
-        reject('Failure');
-    }
-});
-
-p.then(function(result) { 
-	/* do something with the result */
-}).catch(function() {
-	/* error :( */
-}).finally(function() {
-   /* executes regardless or success for failure */ 
-}); 
+new Promise(function(resolve, reject) { 
+	// A mock async action using setTimeout
+	setTimeout(function() { resolve(10); }, 3000);
+})
+.then(function(num) { console.log('first then: ', num); return num * 2; })
+.then(function(num) { console.log('second then: ', num); return num * 2; })
+.then(function(num) { console.log('last then: ', num);});
