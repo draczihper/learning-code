5e4b04369fb663b659ca68a4739625f988b01c3a
Drac Zihper <draczihper@gmail.com>
2024-08-19 23:58:43 +0300

    Add a fetch request

---
+++ webdev/fetch/script.js
@@ -0,0 +1,8 @@
+const URL = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Dar%20es%20Salaam?unitGroup=metric&elements=datetime%2Cname%2Clatitude%2Clongitude%2Ctemp%2Cprecipprob%2Cwindspeed%2Cuvindex%2Csunrise%2Csunset%2Cicon&key=DRV9JB4KZUH55EDM462M3HS4D&contentType=json";
+
+fetch(URL).then(function(response) {
+    const data = response;
+    console.log(data);
+}).catch(function(error) {
+    console.log(error);
+});
