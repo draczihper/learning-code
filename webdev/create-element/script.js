8bfca9577ec239f19a567af8330d10553990c930
Drac Zihper <draczihper@gmail.com>
2024-08-24 23:24:53 +0300

    Add create element js

---
+++ webdev/create-element/script.js
@@ -0,0 +1,24 @@
+const mainEl = document.querySelector("main");
+
+function CreateElement(tag, attributeName = '', attributeValue = '', content = '', parent = null) {
+  this.element = document.createElement(tag);
+
+ if (attributeName && attributeValue) {
+    this.element.setAttribute(attributeName, attributeValue);
+  }
+  
+  if (content) {
+   this.element.textContent = content; 
+  }
+
+if (parent && typeof parent.appendChild === 'function') {
+    parent.appendChild(this.element);
+  }
+  
+  this.getElement = function() {
+    return this.element;
+  };
+}
+
+const containerDiv = new CreateElement("div", "class", "container", "hello", mainEl);
+const h1 = new CreateElement("h1", "", "", "Hello World", containerDiv);
