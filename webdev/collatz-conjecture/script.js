4e5fabba501e196a95f2abd9f6447b29c8e3926c
Drac Zihper <draczihper@gmail.com>
2024-08-29 14:28:08 +0300

    Add collatz conjecture recursive code

---
+++ webdev/collatz-conjecture/script.js
@@ -0,0 +1,12 @@
+// Collatz conjecture pseudocode
+// if n is 1 stop
+// otherwise, is even, repeat this process on n/2
+// otherwise, if n is odd, repeat this process on 3n +1
+
+function collatzRecursive(n) {
+	if (n == 1) return ;
+	else if ( n % 2 == 0)  return 1 + collatzRecursive(n/2);
+	else  return 1 + collatzRecursive(3*n + 1);
+}
+
+console.log(collatzRecursive(3));
