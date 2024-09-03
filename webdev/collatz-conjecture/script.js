9e87ea68d5e71a890e4bc6221675dfe91da1e751
Drac Zihper <draczihper@gmail.com>
2024-09-03 20:53:03 +0300

    Modify collatz recursion

--- webdev/collatz-conjecture/script.js
+++ webdev/collatz-conjecture/script.js
@@ -4,9 +4,9 @@
 // otherwise, if n is odd, repeat this process on 3n +1
 
 function collatzRecursive(n) {
-	if (n == 1) return ;
-	else if ( n % 2 == 0)  return 1 + collatzRecursive(n/2);
-	else  return 1 + collatzRecursive(3*n + 1);
+	if (n == 1) return;
+	else if (n % 2 == 0) return 1 + collatzRecursive(n / 2);
+	else return 1 + collatzRecursive(3 * n + 1);
 }
 
 console.log(collatzRecursive(3));
