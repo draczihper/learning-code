91a058ccfe490137c9b794bd70266cdfa3b7f5ba
Drac Zihper <draczihper@gmail.com>
2024-09-09 12:50:13 +0300

    Add a binary search algorithm

---
+++ webdev/binary-search/script.js
@@ -0,0 +1,19 @@
+// Binary search algorithm
+
+const binarySearch = (arr, target) => {
+	let start = 0;
+	let end = arr.length - 1;
+
+	while (start < end) {
+		let mid = Math.floor((start + end) / 2);
+
+		if (arr[mid] === target) {
+			return `Found the target ${target} at index ${pivot}`;
+		} else if (arr[mid] < target) {
+			start = mid + 1;
+		} else {
+			end = mid - 1;
+		}
+	}
+	return false;
+};
